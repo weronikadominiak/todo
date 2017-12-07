4dc7eaffaf00da1ce32060652dfb74309e815190
Weronika Felczak <weronika.dominiak@hotmail.com>
2017-12-07 17:52:56 +0100

    Element Ref

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'todo-input',
   template: `
-  <input [value]="title" (keyup.enter)="changeTitle($event.target.value)">
-  <button (click)="changeTitle('Button Clicked!')">
+  <input [value]="title" (keyup.enter)="changeTitle($event.target.value)" #inputElement>
+  <button (click)="changeTitle(inputElement.value)">
   Save</button>
   <p>The title is: {{ title }}</p>
   `,
