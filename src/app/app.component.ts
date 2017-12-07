90f853afe949be05b196521c83e1b6bcd20f2928
Weronika Felczak <weronika.dominiak@hotmail.com>
2017-12-07 17:58:54 +0100

    The List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,22 @@ import { Component } from '@angular/core';
   template: `
   <h1>{{title}}</h1>
   <todo-input></todo-input>
+  <ul>
+  <li *ngFor="let item of todoList">
+    {{ item.title }}
+  </li>
+</ul>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'todo';
+  todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
 }
